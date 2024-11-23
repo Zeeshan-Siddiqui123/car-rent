3536b5c82e39a4aefad8a668f341852a4deba225
Zeeshan Siddiqui <zeeshansd767@gmail.com>
2024-11-23 14:19:44 +0500

    Your commit changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { routes } from '../Routes'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import carImage from '../assets/carlogo.png'
 
 const Navbar = () => {
     return (
         <div>
-            <nav className='w-full h-[80px] flex justify-between items-center bg-black'>
-                <div className='nav-img text-white text-lg'>LOGO</div>
+            <nav className='w-full h-[80px] flex justify-between items-center bg-black z-[2px]'>
+                <div className='w-[120px] ml-5 '><img src={carImage} alt="" /></div>
                 <div className='flex gap-4 text-lg'>
                     {routes.map(({ path, label }, index) => (
-                        <Link key={index} to={path} className='text-white'>{label}</Link>
+                        <NavLink key={index} to={path} className={({isActive})=>
+                            isActive ? 'text-sky-500 underline font-bold text-sm': 'text-white text-sm hover:text-sky-500'}>{label}</NavLink>
                     ))}
                 </div>
                 <div></div>
